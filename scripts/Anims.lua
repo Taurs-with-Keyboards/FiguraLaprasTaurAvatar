493a0efcc31fb36a9fedbed461a5f47a7188051e
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-01-26 23:09:36 -0700

    Update Anims.lua
    - Restructured and rewrote `Anims.lua`
    - Cleaner code comments, indents, and spacing

--- scripts/Anims.lua
+++ scripts/Anims.lua
@@ -1,173 +1,167 @@
 -- Required scripts
-local model = require("scripts.ModelParts")
+require("lib.GSAnimBlend")
+local model      = require("scripts.ModelParts")
+local waterTicks = require("scripts.WaterTicks")
+local pose       = require("scripts.Posing")
+local ground     = require("lib.GroundCheck")
 
-local anims     = animations.LaprasTaur
+-- Animations setup
+local anims = animations.LaprasTaur
 
--- Variables
-local pose   = require("scripts.Posing")
-local ticks  = require("scripts.WaterTicks")
-local ground = require("lib.GroundCheck")
+-- Calculate parent's rotations
+local function calculateParentRot(m)
+	
+	local parent = m:getParent()
+	if not parent then
+		return m:getTrueRot()
+	end
+	return calculateParentRot(parent) + m:getTrueRot()
+	
+end
 
 -- Table setup
 local t = {}
 
+-- Animation variables
 t.anim_time = 0
 
--- Base Animations
-do
-	-- Animation variables
-	local time  = 0
-	local _time = time
-	local posCurrent, posNextTick, posTarget, posCurrentPos = 0, 0, 0, 0
-	
-	function events.TICK()
-		-- Animation timer setup
-		_time = time
-		
-		-- Animation velocity control
-		local fbVel     = math.clamp(player:getVelocity():dot((player:getLookDir().x_z):normalize()),         -0.25, 0.25)
-		local lrVel     = math.clamp(math.abs(player:getVelocity():cross(player:getLookDir().x_z:normalize()).y), 0, 0.25)
-		local animSpeed = (fbVel >= -0.05 and math.max(fbVel, lrVel) or math.min(fbVel, lrVel)) * 0.005
-		
-		-- Animation timeline
-		time = time + (animSpeed + (fbVel > -0.05 and 0.0005 or -0.0005))
-		
-		-- Pos lerp
-		posCurrent  = posNextTick
-		posNextTick = math.lerp(posNextTick, posTarget, 0.25)
-	end
-	
-	local basePos = vec(0, 0, 0)
-	function events.RENDER(delta, context)
-		if context == "FIRST_PERSON" or context == "RENDER" or (not client.isHudEnabled() and context ~= "MINECRAFT_GUI") then
-			-- Animation modifiers
-			local vel        = player:getVelocity()
-			local walking    = vel.zx:length() ~= 0
-			local inWater    = ticks.water     < 20
-			local underwater = ticks.under     < 20
-			local onGround   = ground()
-			
-			-- Animation states
-			local groundIdleState     = not walking and (not (inWater or player:getVehicle()) or onGround) and not ((pose.swim and inWater) or pose.elytra) or pose.spin or (pose.climb and vel:length() == 0)
-			local groundWalkState     =     walking and (not (inWater or player:getVehicle()) or onGround) and not ((pose.swim and inWater) or pose.elytra or pose.spin) or (pose.climb and vel:length() ~= 0)
-			local waterIdleState      = not walking and ((inWater or player:getVehicle()) and not onGround) and not underwater
-			local waterSwimState      =     walking and ((inWater or player:getVehicle()) and not onGround) and not underwater
-			local underwaterIdleState = vel:length() == 0 and underwater and (not onGround or pose.swim)
-			local underwaterSwimState = vel:length() ~= 0 and underwater and (not onGround or pose.swim)
-			
-			-- Animation timeline renderer
-			t.anim_time = math.lerp(_time, time, delta)
-			
-			-- Animations
-			anims.groundIdle:playing(groundIdleState)
-			anims.groundWalk:playing(groundWalkState)
-			anims.waterIdle:playing(waterIdleState)
-			anims.waterSwim:playing(waterSwimState)
-			anims.underwaterIdle:playing(underwaterIdleState)
-			anims.underwaterSwim:playing(underwaterSwimState)
-			anims.sleep:playing(pose.sleep)
-			
-			-- Pos state table
-			local statePos = {
-				{ state = pose.climb,   pos = vec(0, 0, 25)  },
-				{ state = pose.elytra,  pos = vec(0, 0, 15)  },
-				{ state = pose.sleep,   pos = vec(0, 0, 15)  },
-				{ state = pose.spin,    pos = vec(0, 0, 16)  },
-				{ state = pose.swim,    pos = vec(0, 20, 15) },
-			}
-			
-			-- Base position check
-			for _, case in ipairs(statePos) do
-				if case.state then
-					basePos = case.pos
-					break
-				else
-					basePos = 0
-				end
-			end
-			
-			-- Pos Lerp
-			posTarget     = basePos
-			posCurrentPos = math.lerp(posCurrent, posNextTick, delta)
-			
-			-- Animation modifiers application
-			local animPos = model.root:getAnimPos()
-			model.root:pos(posCurrentPos + ((pose.swim or pose.climb or pose.crawl) and vec(0, animPos.z - animPos.y, animPos.y - animPos.z) or 0))
-			
-			-- Misc animations
-			local lE = vanilla_model.LEFT_ELYTRA:getOriginRot()
-			local rE = vanilla_model.RIGHT_ELYTRA:getOriginRot()
-			model.frontLeftFlipper:rot(pose.elytra and vec(lE.y, (lE.z * 0.75) + 67.5, lE.x) or nil)
-			model.frontRightFlipper:rot(pose.elytra and vec(rE.y, (rE.z * 0.75) - 67.5, -rE.x) or nil)
-			model.backLeftFlipper:rot(pose.elytra and vec(lE.y, (lE.z * 0.75) + 67.5, lE.x) or nil)
-			model.backRightFlipper:rot(pose.elytra and vec(rE.y, (rE.z * 0.75) - 67.5, -rE.x) or nil)
-		end
-	end
-end
+local time = {
+	prev    = 0,
+	current = 0
+}
 
--- Breathing control
-do
-	local speed     = 0
-	local lastSpeed = 0
-	function events.TICK()
-		lastSpeed = speed
-		speed     = speed + math.clamp((player:getVelocity():length() * 15 + 1) * 0.05, 0, 0.4)
-	end
-	
-	function events.RENDER(delta, context)
-		if context == "RENDER" or context == "FIRST_PERSON" or (not client.isHudEnabled() and context ~= "MINECRAFT_GUI") then
-			local scale = math.sin(math.lerp(lastSpeed, speed, delta)) * 0.0125 + 1.0125
-			model.front.Front:scale(scale)
-		end
-	end
-end
+local breatheTime = {
+	prev    = 0,
+	delta   = 0,
+	current = 0
+}
 
--- Parrot control
-do
-	local parts = {
-		model.main.LeftParrotPivot,
-		model.main.RightParrotPivot,
+local pos = {
+	current    = 0,
+	nextTick   = 0,
+	target     = 0,
+	currentPos = 0
+}
+
+function events.TICK()
+	
+	-- Player variables
+	local vel      = player:getVelocity()
+	local dir      = player:getLookDir()
+	local onGround = ground()
+	
+	-- Directional velocity
+	local fbVel = player:getVelocity():dot((dir.x_z):normalize())
+	local lrVel = player:getVelocity():cross(dir.x_z:normalize()).y
+	local udVel = player:getVelocity().y
+	local diagCancel = math.abs(lrVel) - math.abs(fbVel)
+	
+	-- Store animation variables
+	time.prev        = time.current
+	breatheTime.prev = breatheTime.current
+	
+	-- Animation control
+	time.current        = time.current + math.clamp(fbVel < -0.1 and math.min(fbVel, math.abs(lrVel)) * 0.005 - 0.0005 or math.max(fbVel, math.abs(lrVel)) * 0.005 + 0.0005, -0.003, 0.003)
+	breatheTime.current = breatheTime.current + math.clamp((vel:length() * 15 + 1) * 0.05, 0, 0.4)
+	
+	-- Pos state table
+	local statePos = {
+		{ state = pose.climb,   pos = vec(0, 0, 25)  },
+		{ state = pose.elytra,  pos = vec(0, 0, 15)  },
+		{ state = pose.sleep,   pos = vec(0, 0, 15)  },
+		{ state = pose.spin,    pos = vec(0, 0, 16)  },
+		{ state = pose.swim,    pos = vec(0, 20, 15) },
+		{ state = pose.crawl,   pos = vec(0, 19, 24) },
 	}
 	
-	function events.RENDER(delta, context)
-		if context == "RENDER" or context == "FIRST_PERSON" or (not client.isHudEnabled() and context ~= "MINECRAFT_GUI") then
-			for _, parrot in pairs(parts) do
-				parrot:rot(-model.main:getTrueRot().x__ + -model.lower:getTrueRot().x__)
-			end
+	-- Base position check
+	for _, case in ipairs(statePos) do
+		if case.state then
+			pos.target = case.pos
+			break
+		else
+			pos.target = 0
 		end
 	end
+	
+	-- Tick lerps
+	pos.current  = pos.nextTick
+	pos.nextTick = math.lerp(pos.nextTick, pos.target, 0.25)
+	
+	-- Animation variables
+	local walking    = vel.xz:length() ~= 0
+	local inWater    = waterTicks.water < 20
+	local underwater = waterTicks.under < 20
+	local onGround   = ground()
+	
+	-- Animation states
+	local groundIdle     = not walking and (not (inWater or player:getVehicle()) or onGround) and not ((pose.swim and inWater) or pose.elytra) or pose.spin or (pose.climb and vel:length() == 0)
+	local groundWalk     =     walking and (not (inWater or player:getVehicle()) or onGround) and not ((pose.swim and inWater) or pose.elytra or pose.spin) or (pose.climb and vel:length() ~= 0)
+	local waterIdle      = not walking and ((inWater or player:getVehicle()) and not onGround) and not pose.elytra and not underwater
+	local waterSwim      =     walking and ((inWater or player:getVehicle()) and not onGround) and not pose.elytra and not underwater
+	local underwaterIdle = vel:length() == 0 and underwater and (not onGround or pose.swim) and not pose.elytra
+	local underwaterSwim = vel:length() ~= 0 and underwater and (not onGround or pose.swim) and not pose.elytra
+	local elytra         = pose.elytra
+	local sleep          = pose.sleep
+	
+	-- Animations
+	anims.groundIdle:playing(groundIdle)
+	anims.groundWalk:playing(groundWalk)
+	anims.waterIdle:playing(waterIdle)
+	anims.waterSwim:playing(waterSwim)
+	anims.underwaterIdle:playing(underwaterIdle)
+	anims.underwaterSwim:playing(underwaterSwim)
+	anims.elytra:playing(elytra)
+	anims.sleep:playing(sleep)
+	
+end
+
+function events.RENDER(delta, context)
+	
+	-- Render lerps
+	t.anim_time       = math.lerp(time.prev, time.current, delta)
+	breatheTime.delta = math.lerp(breatheTime.prev, breatheTime.current, delta)
+	pos.currentPos    = math.lerp(pos.current, pos.nextTick, delta)
+	
+	-- Apply
+	local scale = math.sin(breatheTime.delta) * 0.0125 + 1.0125
+	model.front.Front:scale(scale)
+	
+	local animPos = model.root:getAnimPos()
+	model.root:pos(pos.currentPos + ((pose.swim or pose.climb or pose.crawl) and vec(0, animPos.z - animPos.y, animPos.y - animPos.z) or 0))
+	
+	-- Parrot rot offset
+	for _, parrot in pairs(model.parrots) do
+		parrot:rot(-calculateParentRot(parrot:getParent()))
+	end
+	
+end
+
+-- GS Blending Setup
+local blendAnims = {
+	{ anim = anims.groundIdle,     ticks = 7 },
+	{ anim = anims.groundWalk,     ticks = 7 },
+	{ anim = anims.waterIdle,      ticks = 7 },
+	{ anim = anims.waterSwim,      ticks = 7 },
+	{ anim = anims.underwaterIdle, ticks = 7 },
+	{ anim = anims.underwaterSwim, ticks = 7 },
+	{ anim = anims.elytra,         ticks = 7 },
+	{ anim = anims.sleep,          ticks = 7 }
+}
+	
+for _, blend in ipairs(blendAnims) do
+	blend.anim:blendTime(blend.ticks):onBlend("easeOutQuad")
 end
 
 -- Fixing spyglass jank
 function events.RENDER(delta, context)
-	if context == "RENDER" or context == "FIRST_PERSON" or (not client.isHudEnabled() and context ~= "MINECRAFT_GUI") then
-		local rot = vanilla_model.HEAD:getOriginRot()
-		rot.x = math.clamp(rot.x, -90, 30)
-		model.upper.Spyglass:rot(rot)
-			:pos(pose.crouch and vec(0, -4, 0) or nil)
-	end
-end
-
--- GS Blending Setup
-do
-	require("lib.GSAnimBlend")
 	
-	local blendAnims = {
-		{ anim = anims.groundIdle,     ticks = 7, type = "easeOutQuad" },
-		{ anim = anims.groundWalk,     ticks = 7, type = "easeOutQuad" },
-		{ anim = anims.waterIdle,      ticks = 7, type = "easeOutQuad" },
-		{ anim = anims.waterSwim,      ticks = 7, type = "easeOutQuad" },
-		{ anim = anims.underwaterIdle, ticks = 7, type = "easeOutQuad" },
-		{ anim = anims.underwaterSwim, ticks = 7, type = "easeOutQuad" },
-		{ anim = anims.crawl,          ticks = 7, type = "easeOutQuad" },
-		{ anim = anims.sleep,          ticks = 7, type = "easeOutQuad" }
-	}
-	
-	for _, blend in ipairs(blendAnims) do
-		blend.anim:blendTime(blend.ticks):onBlend(blend.type)
-	end
+	local rot = vanilla_model.HEAD:getOriginRot()
+	rot.x = math.clamp(rot.x, -90, 30)
+	model.upper.Spyglass:rot(rot)
+		:pos(pose.crouch and vec(0, -4, 0) or nil)
 	
 end
 
--- Returns table
+-- Returns animation variables
 return t
