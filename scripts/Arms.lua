2654f3f228e768b2ccb9e109ecd9a1927ff896b9
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-06-14 16:45:36 -0700

    Bow Check
    - Added bow to the arm movement checks

--- scripts/Arms.lua
+++ scripts/Arms.lua
@@ -55,6 +55,7 @@ function events.TICK()
 	local using       = player:isUsingItem()
 	local usingL      = activeness == leftActive and leftItem:getUseAction() or "NONE"
 	local usingR      = activeness == rightActive and rightItem:getUseAction() or "NONE"
+	local bow         = using and (usingL == "BOW" or usingR == "BOW")
 	local crossL      = leftItem.tag and leftItem.tag["Charged"] == 1
 	local crossR      = rightItem.tag and rightItem.tag["Charged"] == 1
 	
@@ -62,8 +63,8 @@ function events.TICK()
 	local shouldMove = (pose.swim and not (anims.frontFlip:isPlaying() or anims.backFlip:isPlaying())) or pose.elytra or pose.crawl or pose.climb
 	
 	-- Targets
-	leftArm.target  = (armMove or shouldMove or leftSwing or ((crossL or crossR) or (using and usingL ~= "NONE"))) and 0 or 1
-	rightArm.target = (armMove or shouldMove or rightSwing or ((crossL or crossR) or (using and usingR ~= "NONE"))) and 0 or 1
+	leftArm.target  = (armMove or shouldMove or leftSwing  or bow or ((crossL or crossR) or (using and usingL ~= "NONE"))) and 0 or 1
+	rightArm.target = (armMove or shouldMove or rightSwing or bow or ((crossL or crossR) or (using and usingR ~= "NONE"))) and 0 or 1
 	
 	-- Tick lerps
 	leftArm.current   = leftArm.nextTick
