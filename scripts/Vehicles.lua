9995b8eaa0f7633f979f46d0f13d3838a1264a45
Total <39064108+TotalTakeover@users.noreply.github.com>
2025-06-18 11:25:05 -0700

    Vehicle overhaul

--- scripts/Vehicles.lua
+++ scripts/Vehicles.lua
@@ -1,6 +1,6 @@
 -- Required scripts
-local pokemonParts = require("lib.GroupIndex")(models.LaprasTaur)
-local carrier      = require("lib.GSCarrier")
+local parts   = require("lib.PartsAPI")
+local carrier = require("lib.GSCarrier")
 
 -- GSCarrier rider
 carrier.rider.addRoots(models)
@@ -14,28 +14,28 @@ carrier.rider.controller.setAimEnabled(false)
 carrier.vehicle.addTag("gscarrier:taur", "gscarrier:land", "gscarrier:water")
 
 -- Seat 1
-carrier.vehicle.newSeat("Seat1", pokemonParts.RiderPos1, {
+carrier.vehicle.newSeat("Seat1", parts.group.Seat1, {
 	priority = 3,
 	tags = {["gscarrier:flat"] = true},
-	condition = function() if pokemonParts.Chest:getVisible() then return false end end
+	condition = function() if parts.group.Chest:getVisible() then return false end end
 })
 
 -- Seat 2
-carrier.vehicle.newSeat("Seat2", pokemonParts.RiderPos2, {
+carrier.vehicle.newSeat("Seat2", parts.group.Seat2, {
 	priority = 2,
 	tags = {["gscarrier:flat"] = true},
-	condition = function() if pokemonParts.Chest:getVisible() then return false end end
+	condition = function() if parts.group.Chest:getVisible() then return false end end
 })
 
 -- Seat Chest
-carrier.vehicle.newSeat("SeatChest", pokemonParts.RiderPosChest, {
+carrier.vehicle.newSeat("SeatChest", parts.group.ChestSeat, {
 	priority = 1,
 	tags = {["gscarrier:chair"] = true},
-	condition = function() if not pokemonParts.Chest:getVisible() then return false end end
+	condition = function() if not parts.group.Chest:getVisible() then return false end end
 })
 
 -- Chest Block
-local chest = pokemonParts.Chest:newBlock("Chest")
+local chest = parts.group.Chest:newBlock("Chest")
 	:block("chest")
 	:pos(-8, 0, -8)
 
@@ -43,13 +43,13 @@ function events.TICK()
 	
 	-- Variables
 	local vehicle = player:getVehicle() or false
-	local type    = vehicle and vehicle:getType() or false
+	local type    = vehicle and vehicle:getType() or ""
 	
 	-- Vehicle renders/part toggle
-	renderer:setRenderVehicle(type ~= "minecraft:boat" and type ~= "minecraft:chest_boat")
-	pokemonParts.Chest:visible(type == "minecraft:chest_boat")
+	renderer:setRenderVehicle(not type:find("boat"))
+	parts.group.Chest:visible(type:find("chest"))
 	
 	-- Redirect all passengers to pivots if vehicle is a boat
-	carrier.vehicle.setRedirect(type == "minecraft:boat")
+	carrier.vehicle.setRedirect(type:find("boat"))
 	
 end
